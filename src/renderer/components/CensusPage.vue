2720512e5f170077e0ad3d7940fc6b415694f214
锅岛霖懿 <guodaolinyi@qq.com>
2020-02-09 10:54:53 +0800

    近7天温度数据

--- src/renderer/components/CensusPage.vue
+++ src/renderer/components/CensusPage.vue
@@ -84,81 +84,6 @@
             }).catch(err => {
                 console.log(err);
             });
-            /*  var data = [];
-              var now = +new Date(1997, 9, 3);
-              var oneDay = 24 * 3600 * 1000;
-              var value = Math.random() * 1000;
-              for (var i = 0; i < 1000; i++) {
-                  data.push(randomData());
-              }
-              // 基于准备好的dom，初始化echarts实例
-              var myChart = echarts.init(document.getElementById('echart'));
-
-
-              function randomData() {
-                  now = new Date(+now + oneDay);
-                  value = value + Math.random() * 21 - 10;
-                  return {
-                      name: now.toString(),
-                      value: [
-                          [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'),
-                          Math.round(value)
-                      ]
-                  }
-              }
-
-              setInterval(function () {
-
-                  for (var i = 0; i < 5; i++) {
-                      data.shift();
-                      data.push(randomData());
-                  }
-
-                  myChart.setOption({
-                      series: [{
-                          data: data
-                      }]
-                  });
-              }, 1000);
-              // 绘制图表
-              var option = {
-                  title: {
-                      text: '温度曲线图'
-                  },
-                  tooltip: {
-                      trigger: 'axis',
-                      formatter: function (params) {
-                          params = params[0];
-                          var date = new Date(params.name);
-                          return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' : ' + params.value[1];
-                      },
-                      axisPointer: {
-                          animation: false
-                      }
-                  },
-                  xAxis: {
-                      type: 'time',
-                      splitLine: {
-                          show: false
-                      }
-                  },
-                  yAxis: {
-                      type: 'value',
-                      boundaryGap: [0, '100%'],
-                      splitLine: {
-                          show: false
-                      }
-                  },
-                  series: [{
-                      name: '模拟数据',
-                      type: 'line',
-                      showSymbol: false,
-                      hoverAnimation: false,
-                      data: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-                      // data: data
-                  }]
-              };
-              myChart.setOption(option);*/
             var myChart = echarts.init(document.getElementById('echart'));
             var option = {
                 title: {
@@ -166,7 +91,7 @@
                 },
                 tooltip: {},
                 legend: {
-                    data: ['销量 ']
+                    data: ['温度 ']
                 },
                 xAxis: {
                     data: []
